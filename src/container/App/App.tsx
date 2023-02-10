7fa43b541a39c949afd4aae4dea1fd6f0a1535bf
Oleksandr <aleksandrmigay@gmail.com>
2023-02-10 10:42:42 +0200

    create a base font family by theme in App.tsx

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,18 +1,27 @@
-import { StyledEngineProvider } from "@mui/material/styles";
+import { createTheme, StyledEngineProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import Header from "../header/Header";
 import Main from "../main/Main";
 import Footer from "../footer/Footer";
+import { ThemeProvider } from "@emotion/react";
 
 type Props = {};
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Montserrat, Helvetica, Arial, sans-serif",
+  },
+});
 
 const App = (props: Props) => {
   return (
     <StyledEngineProvider injectFirst>
-      <CssBaseline />
-      <Header />
-      <Main />
-      <Footer />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Header />
+        <Main />
+        <Footer />
+      </ThemeProvider>
     </StyledEngineProvider>
   );
 };
